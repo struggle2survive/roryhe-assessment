6c8ac6c04e0e278ec78dbf371cea07f3388680cc
struggle2survive <hfyrory@outlook.com>
2025-05-24 11:59:00 +0800

    chore: initial config and setup

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = function (isDev) {
+    return {
+        entry: path.resolve(__dirname, "./src/index.tsx"),
+        output: {
+            path: path.resolve(__dirname, "./src/dist"),
+            filename: '[name].[hash:8].js',
+            clean: true,
+            publicPath: "/"
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.jsx', '.js'],
+            alias: {
+                "@styles": path.resolve(__dirname, "src/styles")
+            }
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(tsx|ts|jsx|js)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader"
+                    }
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+                        "css-loader", "sass-loader", "postcss-loader"
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, './src/index.html')
+            }),
+            new MiniCssExtractPlugin({
+                filename: isDev ? "[name].css" : "[name].[contenthash:4].css"
+            })
+        ]
+    }
+}
---
+++ webpack.dev.js
@@ -0,0 +1,14 @@
+const { merge } = require('webpack-merge')
+const path = require('path');
+
+const getBaseConfig = require('./webpack.config')
+
+module.exports = merge(getBaseConfig(true), {
+    mode: "development",
+    devtool: "eval-cheap-module-source-map",
+    devServer: {
+        port: 3000,
+        compress: false,
+        hot: true
+    }
+})
