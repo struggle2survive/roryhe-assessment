d5c7e69624126889629d7ebb38351c2c3acefe6a
struggle2survive <hfyrory@outlook.com>
2025-05-29 02:04:16 +0800

    feat: set up Redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
+import { store } from '@redux/store';
 import App from './App';
 
+
 ReactDOM.createRoot(document.getElementById('app') as Element)
-        .render( <App /> );
+        .render( 
+                <Provider store={store}><App /></Provider>
+        );
